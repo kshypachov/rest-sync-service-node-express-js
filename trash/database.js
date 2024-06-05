acc0fc99adee51ccab11588579e28c2383457001
Roman Lakhnov <lakhnov.roman@gmail.com>
2024-06-05 11:38:05 +0300

    gitignore update

--- trash/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize  = new Sequelize('testdb', 'testuser', 'testpassword', {
-  host: '192.168.177.134',
-  dialect: 'mariadb'
-});
-
-module.exports = sequelize ;
-
