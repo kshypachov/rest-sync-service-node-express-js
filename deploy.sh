14628c0fedc57052bfc1ab8881660d027ba89413
kshypachov <128974084+kshypachov@users.noreply.github.com>
2024-10-30 20:54:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+GREEN="\033[32m"
+RESET="\033[0m"
  
 # Змінні для конфігурації
 REPO_URL="https://github.com/kshypachov/rest-sync-service-node-express-js.git"
@@ -11,42 +15,42 @@ DB_PORT="3306"      # Порт за замовчуванням для MariaDB
 SERVICE_PORT="3000" # Порт, на якому буде запущено сервер
  
 # Встановлення системних залежностей
-echo "Встановлення системних залежностей..."
+echo -e "${GREEN}Встановлення системних залежностей...${RESET}"
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - 
 sudo apt-get update
 sudo apt-get install -y curl libmariadb-dev gcc nodejs git
  
 # Налаштування репозиторію MariaDB
-echo "Налаштування репозиторію MariaDB..."
+echo -e "${GREEN}Налаштування репозиторію MariaDB...${RESET}"
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
  
 # Встановлення MariaDB сервера
-echo "Встановлення MariaDB сервера..."
+echo -e "${GREEN}Встановлення MariaDB сервера...${RESET}"
 sudo apt-get install -y mariadb-server
  
 # Запуск та налаштування MariaDB
-echo "Запуск та налаштування MariaDB..."
+echo -e "${GREEN}Запуск та налаштування MariaDB...${RESET}"
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
  
 # Створення бази даних та користувача
-echo "Створення бази даних та користувача..."
+echo -e "${GREEN}Створення бази даних та користувача...${RESET}"
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 sudo mysql -e "FLUSH PRIVILEGES;"
  
 # Клонування репозиторію
-echo "Клонування репозиторію..."
+echo -e "${GREEN}Клонування репозиторію...${RESET}"
 git clone $REPO_URL
 cd $PROJECT_DIR
  
 # Встановлення залежностей Node.js
-echo "Встановлення залежностей Node.js..."
+echo -e "${GREEN}Встановлення залежностей Node.js...${RESET}"
 npm install
  
 # Створення файлу .env та налаштування змінних середовища
-echo "Створення файлу .env та налаштування змінних середовища..."
+echo -e "${GREEN}Створення файлу .env та налаштування змінних середовища...${RESET}"
 cat > .env << EOL
 # Налаштування сервісу
 PORT=$SERVICE_PORT
@@ -65,11 +69,11 @@ COMBINED_LOG_FILE_NAME='combined.log'
 EOL
  
 # Виконання міграцій бази даних
-echo "Виконання міграцій бази даних..."
+echo -e "${GREEN}Виконання міграцій бази даних...${RESET}"
 npx sequelize db:migrate
  
 # Створення unit файлу для systemd
-echo "Створення unit файлу для systemd..."
+echo -e "${GREEN}Створення unit файлу для systemd...${RESET}"
 sudo bash -c "cat > /etc/systemd/system/rest-sync-service-node-express-js.service" << EOL
 [Unit]
 Description=Node.js RESTful API Service
@@ -88,9 +92,9 @@ WantedBy=multi-user.target
 EOL
  
 # Перезапуск systemd та запуск сервісу
-echo "Перезапуск systemd та запуск сервісу..."
+echo -e "${GREEN}Перезапуск systemd та запуск сервісу...${RESET}"
 sudo systemctl daemon-reload
 sudo systemctl start rest-sync-service-node-express-js
 sudo systemctl enable rest-sync-service-node-express-js
  
-echo "Встановлення завершено! Сервіс запущено та додано в автозапуск."
+echo -e "${GREEN}Встановлення завершено! Сервіс rest-sync-service-node-express-js запущено та додано в автозапуск.${RESET}"
